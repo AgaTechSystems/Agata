1a12739adb99fc63299fcd4115dfe1ace1fdeefd
Agatech <144185076+AgaTechSystems@users.noreply.github.com>
2023-12-28 06:27:50 +0000

    new

---
+++ .github/CMC_API_PRO.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '78fb444b-0374-4fd6-ae8c-732de28e4d41',
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
